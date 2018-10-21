7975655f5063e8e4c943fbecd2617ca3c297b17e
carloslahrssen <carloslahrssen@gmail.com>
2018-10-21 03:02:55 -0400

    added sighthound

--- src/handlers/tags.js
+++ src/handlers/tags.js
@@ -1,26 +1,35 @@
 import { Case } from "../models/case";
 import { removeAllListeners } from "cluster";
+import sitehound from "../services/sitehound";
 
 const index = async (req, res) => {
   const allCases = await Case.find();
   return res.status(200).send({ success: true, data: { cases: allCases } });
 };
 
-const create = (req, res) => {
+const create = async (req, res) => {
   const { tag, lat, lng, img } = req.body;
 
-  let newCase = new Case({
-    lat,
-    lng,
-    make: "VW",
-    color: "Blue",
-    tag,
-    img
-  });
+  try {
+    const imagePayload = await sitehound.read({ image: img });
 
-  newCase.save();
+    console.log(imagePayload);
 
-  return res.status(204).send();
+    // let newCase = new Case({
+    //   lat,
+    //   lng,
+    //   make: "VW",
+    //   color: "Blue",
+    //   tag,
+    //   img
+    // });
+
+    // newCase.save();
+
+    return res.status(204).send();
+  } catch (e) {
+    return res.status(400).send({ e });
+  }
 };
 
 export default { index, create };
