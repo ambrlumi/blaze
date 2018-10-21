3a0068545e98ea6c90e0a4277aef8ad47d0b6013
carloslahrssen <carloslahrssen@gmail.com>
2018-10-21 05:04:15 -0400

    added amber schema

---
+++ src/services/amber.js
@@ -0,0 +1,18 @@
+//data comes in so infrequently that it makes no sense to scrape now. Is pending;
+const amber = [
+   {
+      make: "",
+      color: "",
+      tag: "",
+   },
+   {
+      make: "",
+      color: "",
+      tag: "",
+   },
+   {
+      make: "",
+      color: "",
+      tag: "",
+   }
+]
